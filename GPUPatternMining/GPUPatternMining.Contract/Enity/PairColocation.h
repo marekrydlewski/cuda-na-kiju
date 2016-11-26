07b2e070e2ce8c6ce47b55c5984f8b195c675335
kosiacz3q <kosiak.lukasz@gmail.com>
2016-11-26 14:59:30 +0100

    Pair colocation struct extended

--- GPUPatternMining/GPUPatternMining.Contract/Enity/PairColocation.h
+++ GPUPatternMining/GPUPatternMining.Contract/Enity/PairColocation.h
@@ -2,6 +2,27 @@
 
 __declspec(align(16)) struct PairColocation
 {
+	PairColocation() 
+		:firstType(0), firstId(0), secondType(0), secondId(0)
+	{
+		
+	};
+	
+	PairColocation(int firstType, int firstId, int secondType, int secondId)
+		: firstType(firstType), firstId(firstId), secondType(secondType), secondId(secondId)
+	{
+
+	}
+
+	bool operator==(const PairColocation& other) const
+	{
+		return
+			firstType == other.firstType
+			&& firstId == other.firstId
+			&& secondType == other.secondType
+			&& secondId == other.secondId;
+	}
+
 	int firstType;
 	int firstId;
 	int secondType;

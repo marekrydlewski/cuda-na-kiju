46e1f9979ef04dd4ae9014a0e71cbbf8e407d96a
kosiacz3q <kosiak.lukasz@gmail.com>
2016-12-08 04:01:17 +0100

    Distance treshold added

--- GPUPatternMining/GPUPatternMining/PlaneSweep/PlaneSweepFoxtrot.cu
+++ GPUPatternMining/GPUPatternMining/PlaneSweep/PlaneSweepFoxtrot.cu
@@ -7,12 +7,13 @@ namespace PlaneSweep
 	namespace Foxtrot
 	{
 		/*
-			xCoords		- table with instances x coordinates in ascending order
-			yCoords		- table with instances y coordinates
-			types		- table with instances types
-			ids			- table with instances ids
-			count		- count of instances
-			hashMapper	- type pair map to list to those types pairs colocations instances that stores result
+			xCoords				- table with instances x coordinates in ascending order
+			yCoords				- table with instances y coordinates
+			types				- table with instances types
+			ids					- table with instances ids
+			count				- count of instances
+			maxDistance			- maximal distance between two instances for concidering them as neigbours (inclusive <=)
+			hashMapper			- type pair map to list to those types pairs colocations instances that stores result
 
 			tables are treated as zipped (tables values for specified id belong to one instance)
 		*/
@@ -23,6 +24,7 @@ namespace PlaneSweep
 			, Idx* types
 			, Idx* ids
 			, unsigned int count
+			, float distanceTreshold
 			, GPUHashMapper<Idx, Ptx, GPUKeyProcessor<Idx>>& hashMapper)
 		{
 			// not implemented yet

befcbc0832e9fa5bc724ec61b5bd5329feb543d2
kosiacz3q <kosiak.lukasz@gmail.com>
2016-12-11 17:53:08 +0100

    countNeighbours function declaration and test added

--- GPUPatternMining/GPUPatternMining.Tests/Tests/PlaneSweepFoxtrotTests.cu
+++ GPUPatternMining/GPUPatternMining.Tests/Tests/PlaneSweepFoxtrotTests.cu
@@ -22,12 +22,13 @@ TEST_CASE_METHOD(BaseCudaTestHandler, "check first neighbours list element (neig
 	unsigned int type[] = { 0xA, 0xB, 0xC, 0xB, 0xA, 0xC };
 	unsigned int ids[] = {1, 1, 1, 2, 2, 2 };
 	unsigned int instancesCount = 6;
+	unsigned int distanceTreshold = 1;
 
 	GPUUIntKeyProcessor *intKeyProcessor = new GPUUIntKeyProcessor();
 
 	UIntTableGpuHashMap hashMap(6, intKeyProcessor);
 
-	PlaneSweep::Foxtrot::PlaneSweep<float>(x, y, type, ids, instancesCount, hashMap);
+	PlaneSweep::Foxtrot::PlaneSweep<float>(x, y, type, ids, instancesCount, distanceTreshold, hashMap);
 
 	unsigned int h_resultKeys[] = { 0x000A000B, 0x000A000C, 0x000B000C };
 	unsigned int* c_resultKey;
@@ -53,3 +54,48 @@ TEST_CASE_METHOD(BaseCudaTestHandler, "check first neighbours list element (neig
 	cudaMemcpy(&h_result, d_results[3], uintSize, cudaMemcpyDeviceToHost);
 	REQUIRE(h_result == 2); // |B-C|
 }
+// ----------------------------------------------------------------------------
+
+TEST_CASE_METHOD(BaseCudaTestHandler, "check countNeighbours function", "PlaneSweep")
+{
+	float hX[] = { 1,2,3,4,5,6 };
+	float hY[] = { 1,2,3,4,5,6 };
+	UInt hTypes[] = { 0xA, 0xB, 0xC, 0xB, 0xA, 0xC };
+	UInt hIds[] = { 1, 1, 1, 2, 2, 2 };
+	UInt instancesCount = 6;
+	float distanceTreshold = 1;
+
+	// tranfering data from host memory to device memory
+	float* cX;
+	float* cY;
+	UInt* cType;
+	UInt* cIds;
+	UInt* cResults;
+
+	cudaMalloc(reinterpret_cast<void**>(&cX)		, 6 * sizeof(float));
+	cudaMalloc(reinterpret_cast<void**>(&cY)		, 6 * sizeof(float));
+	cudaMalloc(reinterpret_cast<void**>(&cType)		, 6 * uintSize);
+	cudaMalloc(reinterpret_cast<void**>(&cIds)		, 6 * uintSize);
+	cudaMalloc(reinterpret_cast<void**>(&cResults)	, 6 * uintSize);
+
+	cudaMemcpy(cX	, hX	, 6 * sizeof(float)	, cudaMemcpyHostToDevice);
+	cudaMemcpy(cY	, hY	, 6 * sizeof(float)	, cudaMemcpyHostToDevice);
+	cudaMemcpy(cType, hTypes, 6 * uintSize		, cudaMemcpyHostToDevice);
+	cudaMemcpy(cIds	, hIds	, 6 * uintSize		, cudaMemcpyHostToDevice);
+
+	PlaneSweep::Foxtrot::countNeighbours<<< 32, 1>>> (cX, cY, cType, cIds, instancesCount, distanceTreshold, cResults);
+
+	UInt hExpected[] = { 1, 2, 2, 2, 2, 1 };
+	UInt hResults[6];
+
+	cudaMemcpy(hResults, cResults, instancesCount, cudaMemcpyDeviceToHost);
+	
+	REQUIRE(std::equal(std::begin(hExpected), std::end(hExpected), hResults));
+
+	cudaFree(cX);
+	cudaFree(cY);
+	cudaFree(cType);
+	cudaFree(cIds);
+	cudaFree(cResults);
+}
+// ----------------------------------------------------------------------------
--- GPUPatternMining/GPUPatternMining/PlaneSweep/PlaneSweepFoxtrot.cu
+++ GPUPatternMining/GPUPatternMining/PlaneSweep/PlaneSweepFoxtrot.cu
@@ -2,6 +2,8 @@
 
 #include "../HashMap/gpuhashmapper.h"
 
+#include "../MiningCommon.h"
+
 namespace PlaneSweep
 {
 	namespace Foxtrot
@@ -11,23 +13,55 @@ namespace PlaneSweep
 			yCoords				- table with instances y coordinates
 			types				- table with instances types
 			ids					- table with instances ids
+			distanceTreshold	- maximal distance between two instances for concidering them as neigbours (inclusive <=)
 			count				- count of instances
-			maxDistance			- maximal distance between two instances for concidering them as neigbours (inclusive <=)
+
+			neighboursCount		- result table that contains number of neigbours for each feature instance
+
+			tables are treated as zipped (tables values for specified id belong to one instance)
+		*/
+		template <class Txy = float, class Idx = unsigned int>
+		__global__ void countNeighbours(
+			Txy* xCoords
+			, Txy* yCoords
+			, Idx* types
+			, Idx* ids
+			, UInt count
+			, Txy dis
+			, UInt* neighboursCount)
+		{
+			// not implemented yet
+		}
+		
+		/*
+			xCoords				- table with instances x coordinates in ascending order
+			yCoords				- table with instances y coordinates
+			types				- table with instances types
+			ids					- table with instances ids
+			count				- count of instances
+			distanceTreshold	- maximal distance between two instances for concidering them as neigbours (inclusive <=)
 			hashMapper			- type pair map to list to those types pairs colocations instances that stores result
 
 			tables are treated as zipped (tables values for specified id belong to one instance)
 		*/
-		template <class Txy, class Idx = unsigned int, class Ptx=unsigned int*>
+		template <class Txy = float, class Idx = unsigned int, class Ptx=unsigned int*>
 		__host__ void PlaneSweep(
 			Txy* xCoords
 			, Txy* yCoords
 			, Idx* types
 			, Idx* ids
-			, unsigned int count
-			, float distanceTreshold
+			, UInt count
+			, Txy distanceTreshold
 			, GPUHashMapper<Idx, Ptx, GPUKeyProcessor<Idx>>& hashMapper)
 		{
-			// not implemented yet
+			UInt* K;
+			UInt* V;
+
+			UInt* neighboursCount;
+
+
+			UInt* I;
+			UInt* A;
 		}
 	}
 }

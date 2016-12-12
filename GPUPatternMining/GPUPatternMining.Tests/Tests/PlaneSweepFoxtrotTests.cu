c2422e348bf36afe4a0c5e524ce41b8335d21570
kosiacz3q <kosiak.lukasz@gmail.com>
2016-12-12 05:21:54 +0100

    Initial implementation of planesweep

--- GPUPatternMining/GPUPatternMining.Tests/Tests/PlaneSweepFoxtrotTests.cu
+++ GPUPatternMining/GPUPatternMining.Tests/Tests/PlaneSweepFoxtrotTests.cu
@@ -15,6 +15,7 @@
 
 	A1-B1-C1-B2-A2-C2
 */
+/*
 TEST_CASE_METHOD(BaseCudaTestHandler, "check first neighbours list element (neigbours count)", "PlaneSweep")
 {
 	float x[] = { 1,2,3,4,5,6 };
@@ -55,15 +56,17 @@ TEST_CASE_METHOD(BaseCudaTestHandler, "check first neighbours list element (neig
 	REQUIRE(h_result == 2); // |B-C|
 }
 // ----------------------------------------------------------------------------
+*/
 
 TEST_CASE_METHOD(BaseCudaTestHandler, "check countNeighbours function", "PlaneSweep")
 {
 	float hX[] = { 1,2,3,4,5,6 };
-	float hY[] = { 1,2,3,4,5,6 };
+	float hY[] = { 1,1,1,1,1,1 };
 	UInt hTypes[] = { 0xA, 0xB, 0xC, 0xB, 0xA, 0xC };
 	UInt hIds[] = { 1, 1, 1, 2, 2, 2 };
 	UInt instancesCount = 6;
-	float distanceTreshold = 1;
+	float distanceTreshold = 1.1;
+	float distanceTresholdSquared = 1.1 * 1.1;
 
 	// tranfering data from host memory to device memory
 	float* cX;
@@ -83,12 +86,20 @@ TEST_CASE_METHOD(BaseCudaTestHandler, "check countNeighbours function", "PlaneSw
 	cudaMemcpy(cType, hTypes, 6 * uintSize		, cudaMemcpyHostToDevice);
 	cudaMemcpy(cIds	, hIds	, 6 * uintSize		, cudaMemcpyHostToDevice);
 
-	PlaneSweep::Foxtrot::countNeighbours<<< 32, 1>>> (cX, cY, cType, cIds, instancesCount, distanceTreshold, cResults);
+
+	dim3 grid;
+	int wc = 6;
+	findSmallest2D(wc * 32, 256, grid.x, grid.y);
+
+	PlaneSweep::Foxtrot::countNeighbours<<< grid, 256>>> (cX, cY, cType, cIds, instancesCount, distanceTreshold, distanceTresholdSquared, cResults, wc);
 
 	UInt hExpected[] = { 1, 2, 2, 2, 2, 1 };
 	UInt hResults[6];
 
 	cudaMemcpy(hResults, cResults, instancesCount, cudaMemcpyDeviceToHost);
+
+	for (int i = 0; i < 6; ++i)
+		printf("%d = %u\n", i, hResults[i]);
 	
 	REQUIRE(std::equal(std::begin(hExpected), std::end(hExpected), hResults));
 
--- GPUPatternMining/GPUPatternMining/PlaneSweep/PlaneSweepFoxtrot.cu
+++ GPUPatternMining/GPUPatternMining/PlaneSweep/PlaneSweepFoxtrot.cu
@@ -1,13 +1,24 @@
 #pragma once
 
+#include <device_functions.hpp>
+
+#include "thrust/reduce.h"
+
 #include "../HashMap/gpuhashmapper.h"
 
 #include "../MiningCommon.h"
+
 
 namespace PlaneSweep
 {
 	namespace Foxtrot
 	{
+		template<class Tx>
+		__device__ float distance(Tx x1, Tx y1, Tx x2, Tx y2)
+		{
+			return (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2);
+		}
+
 		/*
 			xCoords				- table with instances x coordinates in ascending order
 			yCoords				- table with instances y coordinates
@@ -26,13 +37,143 @@ namespace PlaneSweep
 			, Txy* yCoords
 			, Idx* types
 			, Idx* ids
-			, UInt count
-			, Txy dis
-			, UInt* neighboursCount)
+			, int count
+			, Txy radius
+			, Txy radiusSquared
+			, UInt* resultNeighboursCount
+			, int warpsCount)
 		{
-			// not implemented yet
+			// int warpsCount	  = gridDim.x * blockDim.x / 32;
+			int globalTid = computeLinearAddressFrom2D(); // blockIdx.x  * blockDim.x + threadIdx.x;
+			int globalWarpId = globalTid / 32;
+			int warpThreadId = threadIdx.x % 32;
+			int blockThreadId = threadIdx.x;
+			int blockWarpId = blockThreadId / 32;
+
+			__shared__ volatile bool * flags;
+			__shared__ volatile UInt * found;
+
+			if (threadIdx.x == 0)
+			{
+				flags = static_cast<bool*>(malloc((blockDim.x / 32) * sizeof(bool)));
+				found = static_cast<UInt*>(malloc(blockDim.x * uintSize));
+			}
+
+			__syncthreads();
+
+			found[blockThreadId] = 0;
+
+			//uint start= wid        * ((inSize-1 ) / warpCount ) + max(0, - warpCount  + wid          + (inSize - 1) % warpCount ) + 1;
+			int start = globalWarpId * ((count - 1) / warpsCount) + max(0, - warpsCount + globalWarpId + (count  - 1) % warpsCount) + 1;
+			//uint stop=(wid         + 1) * ((inSize-1 ) / warpCount ) + max(0, -warpCount  +  (inSize- 1) % warpCount  + wid          + 1);
+			int stop = (globalWarpId + 1) * ((count - 1) / warpsCount) + max(0, -warpsCount +  (count - 1) % warpsCount + globalWarpId + 1);
+			
+			/*
+			int W = count / 32;
+			// int startSegment = wid*(W / warpCount)   + max(0, wid          - (warpCount  -  W % warpCount));
+			int start = globalWarpId * (W / warpsCount) + max(0, globalWarpId - (warpsCount - (W % warpsCount)));
+
+			// int endSegment=(wid+1)*(W/warpCount)          + max(0, 1 + wid          - (warpCount  - W % warpCount )) -1;
+			int stop = (globalWarpId + 1) * (W / warpsCount) + max(0, 1 + globalWarpId + (warpsCount - W % warpsCount)) -1;
+			*/
+
+			if (globalWarpId < warpsCount)
+			{//First warp				
+				found[blockThreadId] = 0;
+
+				for (UInt i = start; i <= stop; i++)
+				{
+					float px = xCoords[i];
+					float py = yCoords[i];
+
+					flags[blockWarpId] = false;
+
+					for (int j = i - 32; j >= -32; j -= 32)
+					{
+						int localId = warpThreadId + j;
+						if (localId >= 0)
+						{
+							float lx = xCoords[localId];
+
+							if ((px - lx)>radius)
+							{
+								flags[blockWarpId] = true;
+							}
+
+							float ly = yCoords[localId];
+							if ((distance(px, py, lx, ly) <= radiusSquared) && (ids[i] != ids[localId]))
+							{
+								found[blockThreadId] += 1;
+							}
+						}
+
+						if (flags[blockWarpId])
+						{
+							break;
+						}
+					}
+
+				}
+
+				atomicAdd(&resultNeighboursCount[globalWarpId], found[blockThreadId]);
+				/*
+				intraWarpReduce<thrust::plus<UInt>, 6>(found, thrust::plus<UInt>());
+
+				if (globalTid == globalWarpId * 32)
+				{
+					resultNeighboursCount[globalWarpId] = found[blockWarpId * 32 + 31];
+				}*/
+			}
+
+
+			/*
+
+			for (UInt i = start; i <= end; ++i)
+			{
+				if (warpThreadId == 0)
+					flags[blockWarpId] = false;
+
+				int j = i - 32;
+
+				while (j >= -32)
+				{
+					int comparedId = warpThreadId + j;
+
+					if (comparedId >= 0)
+					{
+						if (xCoords[i] - xCoords[comparedId] > distanceTreshold)
+							flags[blockWarpId] = true;
+
+						float sqrtRes = sqrt(pow(xCoords[i] - xCoords[comparedId], 2) + pow(yCoords[i] - yCoords[comparedId], 2));
+
+						if (sqrtRes <= distanceTreshold)
+							found[blockThreadId] += 1;
+					}
+
+					if (flags[blockWarpId])
+						break;
+
+					j -= 32;
+				}
+			}
+
+			atomicAdd(&resultNeighboursCount[globalWarpId], found[blockThreadId]);
+
+			
+			*/
+
+			__syncthreads();
+
+			if (threadIdx.x == 0)
+			{
+				free(const_cast<bool*>(flags));
+				free(const_cast<UInt*>(found));
+			}
+
+			__syncthreads();
 		}
-		
+		// --------------------------------------------------------------------------------------------------------------------------------------
+
 		/*
 			xCoords				- table with instances x coordinates in ascending order
 			yCoords				- table with instances y coordinates
@@ -63,5 +204,6 @@ namespace PlaneSweep
 			UInt* I;
 			UInt* A;
 		}
+		// --------------------------------------------------------------------------------------------------------------------------------------
 	}
 }

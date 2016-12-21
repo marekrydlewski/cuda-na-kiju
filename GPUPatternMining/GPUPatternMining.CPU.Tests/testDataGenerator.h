a2743b764993479bfb4b4998c69e30f1d19df0cd
Marcin Jabłoński <jablon1994@gmail.com>
2016-12-21 11:59:01 +0100

    Add comments

--- GPUPatternMining/GPUPatternMining.CPU.Tests/testDataGenerator.h
+++ GPUPatternMining/GPUPatternMining.CPU.Tests/testDataGenerator.h
@@ -13,11 +13,11 @@ public:
 	///6 vertices, 2 types, 4 instances of type0 and 2 instances of type1, intances 0 and 1 of both types
 	///in neighbourhood (t0i0 with t1i0 and t0i1 with t1i1)
 	DataFeed* getDataForPrevalenceTests();
-
+	///2 vertices, 2 types, neighbourhood relationship
 	DataFeed* getDataForMaximalCliqueSize2();
-
+	///2 vertices, 2 types, no neighbourhood relationship
 	DataFeed* getDataForMaximalCliqueSize1();
-
+	///4 vertices, 4 types, neighbourhood relationship between everyone
 	DataFeed* getDataForMaximalCliqueSize4();
 	TestDataGenerator();
 	~TestDataGenerator();

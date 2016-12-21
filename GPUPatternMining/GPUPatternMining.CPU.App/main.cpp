97866a9a693ffc4da5016d19a7cfcbec8f90a85b
Marek Rydlewski <marek.rydlewski@hotmail.com>
2016-12-21 19:08:07 +0100

    Add new project

---
+++ GPUPatternMining/GPUPatternMining.CPU.App/main.cpp
@@ -0,0 +1,29 @@
+#include "../GPUPatternMining.Contract/RandomDataProvider.h"
+#include "../GPUPatternMining.Contract/Graph.h"
+#include "../GPUPatternMining.CPU/CPUMiningAlgorithms/CPUMiningAlgorithmSeq.h"
+
+int main()
+{
+	const unsigned int types = 5;
+	const unsigned int rangeY = 100;
+	const unsigned int rangeX = 100;
+	const unsigned int numberOfInstances = 50;
+	const float threshold = 20;
+	const float prevalence = 0.20;
+
+	RandomDataProvider rdp;
+
+	rdp.setNumberOfTypes(types);
+	rdp.setRange(rangeX, rangeY);
+
+	auto data = rdp.getData(numberOfInstances);
+
+	CPUMiningAlgorithmSeq cpuAlgSeq;
+	cpuAlgSeq.loadData(data, numberOfInstances, types);
+	cpuAlgSeq.filterByDistance(threshold);
+	cpuAlgSeq.filterByPrevalence(prevalence);
+	cpuAlgSeq.constructMaximalCliques();
+	cpuAlgSeq.filterMaximalCliques();
+
+	return 0;
+}

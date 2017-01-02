86a1290f46d02fd7cfe88a8c1a5ce67df0f3dab9
Marcin Jabłoński <jablon1994@gmail.com>
2017-01-02 20:41:18 +0100

    Basic tree tests

--- GPUPatternMining/GPUPatternMining.CPU.Tests/TreeConstructionTests.cpp
+++ GPUPatternMining/GPUPatternMining.CPU.Tests/TreeConstructionTests.cpp
@@ -2,3 +2,38 @@
 #include "../GPUPatternMining.CPU/CPUMiningAlgorithms/CPUMiningAlgorithmSeq.h"
 #include "../GPUPatternMining.CPU.Tests/testDataGenerator.h"
 
+TEST_CASE("2DistinctColocations", "TreeConstructionTests")
+{
+	TestDataGenerator generator;
+	CPUMiningAlgorithmSeq miner;
+	int threshold = 5;
+	float prevalence = 0.1;
+
+	auto data = generator.getDataForDistinctCliques();
+	miner.loadData(data, 5, 5);
+	miner.filterByDistance(threshold);
+	miner.filterByPrevalence(prevalence);
+	miner.constructMaximalCliques();
+
+	auto result = miner.filterMaximalCliques(prevalence);
+
+	REQUIRE(result.size() == 2);
+}
+
+TEST_CASE("2OverlappingColocations", "TreeConstructionTests")
+{
+	TestDataGenerator generator;
+	CPUMiningAlgorithmSeq miner;
+	int threshold = 5;
+	float prevalence = 0.1;
+
+	auto data = generator.getDataFor2OverlappingCliques();
+	miner.loadData(data, 5, 5);
+	miner.filterByDistance(threshold);
+	miner.filterByPrevalence(prevalence);
+	miner.constructMaximalCliques();
+
+	auto result = miner.filterMaximalCliques(prevalence);
+
+	REQUIRE(result.size() == 2);
+}

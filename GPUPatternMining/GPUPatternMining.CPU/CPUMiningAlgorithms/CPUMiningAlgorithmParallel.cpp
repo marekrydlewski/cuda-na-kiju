16775bb72416b224ba439a5d2933fbdb00a05b6a
Marcin Jabłoński <jablon1994@gmail.com>
2017-01-06 12:41:27 +0100

    Stuck at insTable reduction

--- GPUPatternMining/GPUPatternMining.CPU/CPUMiningAlgorithms/CPUMiningAlgorithmParallel.cpp
+++ GPUPatternMining/GPUPatternMining.CPU/CPUMiningAlgorithms/CPUMiningAlgorithmParallel.cpp
@@ -22,13 +22,19 @@ void CPUMiningAlgorithmParallel::filterByDistance(float threshold)
 	float effectiveThreshold = pow(threshold, 2);
 
 	Concurrency::combinable<std::vector<int>> threadTypeIncidenceCounter;
+	Concurrency::combinable<std::map<unsigned int, std::map<unsigned int, std::map<unsigned int, std::vector<unsigned int>*>>>> threadInsTable;
+
 	//this is going to be parallelized - add parallel_for here
 	{
 		threadTypeIncidenceCounter.local().resize(typeIncidenceCounter.size());
 		//when parallelized add remaining load to last process (occurs when source.size() % GetProcessorCount() != 0)
 		unsigned long long int loadPerProcessor = source.size() / concurrency::GetProcessorCount();
 
-		for (auto it1 = source.begin(); (it1 != source.end()); ++it1)
+		//second part needs alteration - note that if you add load at last process it will be wrong, think about it
+		//0 needs to be changed to parallel_for index
+		std::vector<DataFeed>::iterator beginIterator = source.begin() + 0 * loadPerProcessor;
+
+		for (auto it1 = beginIterator; (it1 != source.end()); ++it1)
 		{
 			++threadTypeIncidenceCounter.local()[(*it1).type];
 
@@ -45,11 +51,11 @@ void CPUMiningAlgorithmParallel::filterByDistance(float threshold)
 						if ((*it1_h).type > (*it2_h).type)
 							std::swap(it1_h, it2_h);
 
-						if (insTable[(*it1_h).type][(*it2_h).type][(*it1_h).instanceId] == nullptr)
+						if (threadInsTable.local()[(*it1_h).type][(*it2_h).type][(*it1_h).instanceId] == nullptr)
 						{
-							insTable[(*it1_h).type][(*it2_h).type][(*it1_h).instanceId] = new std::vector<unsigned int>();
+							threadInsTable.local()[(*it1_h).type][(*it2_h).type][(*it1_h).instanceId] = new std::vector<unsigned int>();
 						}
-						insTable[(*it1_h).type][(*it2_h).type][(*it1_h).instanceId]->push_back((*it2_h).instanceId);
+						threadInsTable.local()[(*it1_h).type][(*it2_h).type][(*it1_h).instanceId]->push_back((*it2_h).instanceId);
 					}
 				}
 			}
@@ -61,6 +67,15 @@ void CPUMiningAlgorithmParallel::filterByDistance(float threshold)
 		for (int i = 0; i < left.size(); ++i)
 		{
 			left[i] += right[i];
+		}
+		return left;
+	});
+
+	insTable = threadInsTable.combine([](std::map<unsigned int, std::map<unsigned int, std::map<unsigned int, std::vector<unsigned int>*>>> left, std::map<unsigned int, std::map<unsigned int, std::map<unsigned int, std::vector<unsigned int>*>>> right)->std::map<unsigned int, std::map<unsigned int, std::map<unsigned int, std::vector<unsigned int>*>>> {
+		for (auto it = right.begin(); (it != right.end()); ++it)
+		{
+			//damn, hard.
+			//note: add to left (as left is returned)
 		}
 		return left;
 	});

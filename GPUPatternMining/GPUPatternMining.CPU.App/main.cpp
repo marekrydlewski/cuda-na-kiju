cb1dc8b07cc728f5cc6a041cbe9c784d59f32a78
Marek Rydlewski <marek.rydlewski@hotmail.com>
2017-01-09 17:03:40 +0100

    Change timer

--- GPUPatternMining/GPUPatternMining.CPU.App/main.cpp
+++ GPUPatternMining/GPUPatternMining.CPU.App/main.cpp
@@ -7,43 +7,54 @@
 
 int main()
 {
+	//input data
 	const unsigned int types = 5;
 	const unsigned int rangeY = 300;
 	const unsigned int rangeX = 300;
-	const unsigned int numberOfInstances = 2000;
+	const unsigned int numberOfInstances = 4000;
 	const float threshold = 20;
 	const float prevalence = 0.2;
 
 	RandomDataProvider rdp;
-
 	rdp.setNumberOfTypes(types);
 	rdp.setRange(rangeX, rangeY);
-
 	auto data = rdp.getData(numberOfInstances);
 
+	//output data
 	std::vector<std::vector<unsigned int>> solutionSeq, solutionParallel;
 
-	std::cout << measure<std::chrono::nanoseconds>::execution([&]()
-	{
-		CPUMiningAlgorithmSeq cpuAlgSeq;
-		cpuAlgSeq.loadData(data, numberOfInstances, types);
-		cpuAlgSeq.filterByDistance(threshold);
-		cpuAlgSeq.filterByPrevalence(prevalence);
-		cpuAlgSeq.constructMaximalCliques();
-		solutionSeq = cpuAlgSeq.filterMaximalCliques(prevalence);
-		std::cout << "CPU Seq: ";
-	}) << std::endl;
+	//benchmarking
+	CPUMiningAlgorithmSeq cpuAlgSeq;
+	CPUMiningAlgorithmParallel cpuAlgParallel;
 
-	std::cout << measure<std::chrono::nanoseconds>::execution([&]()
-	{
-		CPUMiningAlgorithmParallel cpuAlgParallel;
-		cpuAlgParallel.loadData(data, numberOfInstances, types);
-		cpuAlgParallel.filterByDistance(threshold);
-		cpuAlgParallel.filterByPrevalence(prevalence);
-		cpuAlgParallel.constructMaximalCliques();
-		solutionParallel = cpuAlgParallel.filterMaximalCliques(prevalence);
-		std::cout << "CPU Parallel: ";
-	}) << std::endl;
+	std::cout << tim::measure<std::chrono::nanoseconds>::execution(cpuAlgSeq.loadData, data, numberOfInstances, types) << std::endl;
+	std::cout << tim::measure<std::chrono::nanoseconds>::execution(cpuAlgSeq.loadData, data, numberOfInstances, types) << std::endl;
+	std::cout << tim::measure<std::chrono::nanoseconds>::execution(cpuAlgSeq.loadData, data, numberOfInstances, types) << std::endl;
+	std::cout << tim::measure<std::chrono::nanoseconds>::execution(cpuAlgSeq.loadData, data, numberOfInstances, types) << std::endl;
+	std::cout << tim::measure<std::chrono::nanoseconds>::execution(cpuAlgSeq.loadData, data, numberOfInstances, types) << std::endl;
+
+
+	//std::cout << measure<std::chrono::nanoseconds>::execution([&]()
+	//{
+	//	CPUMiningAlgorithmSeq cpuAlgSeq;
+	//	cpuAlgSeq.loadData(data, numberOfInstances, types);
+	//	cpuAlgSeq.filterByDistance(threshold);
+	//	cpuAlgSeq.filterByPrevalence(prevalence);
+	//	cpuAlgSeq.constructMaximalCliques();
+	//	solutionSeq = cpuAlgSeq.filterMaximalCliques(prevalence);
+	//	std::cout << "CPU Seq: ";
+	//}) << std::endl;
+
+	//std::cout << measure<std::chrono::nanoseconds>::execution([&]()
+	//{
+	//	CPUMiningAlgorithmParallel cpuAlgParallel;
+	//	cpuAlgParallel.loadData(data, numberOfInstances, types);
+	//	cpuAlgParallel.filterByDistance(threshold);
+	//	cpuAlgParallel.filterByPrevalence(prevalence);
+	//	cpuAlgParallel.constructMaximalCliques();
+	//	solutionParallel = cpuAlgParallel.filterMaximalCliques(prevalence);
+	//	std::cout << "CPU Parallel: ";
+	//}) << std::endl;
 
 	return 0;
 }

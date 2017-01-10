62e074a0b58232104257e4c1b7119e8c08afeb9a
Marcin Jabłoński <jablon1994@gmail.com>
2017-01-10 20:33:03 +0100

    Different approach && comment

--- GPUPatternMining/GPUPatternMining.CPU/CPUMiningAlgorithms/CPUMiningAlgorithmSeq.cpp
+++ GPUPatternMining/GPUPatternMining.CPU/CPUMiningAlgorithms/CPUMiningAlgorithmSeq.cpp
@@ -389,7 +389,10 @@ std::vector<std::vector<unsigned int>> CPUMiningAlgorithmSeq::getPrevalentMaxCli
 			for (auto c : smallerCliques)
 			{
 				if (c.size() == 2) //no need to construct tree, already checked by filterByPrevalence
-					finalMaxCliques.push_back(c);
+				{
+					finalMaxCliques.insert(finalMaxCliques.end(), smallerCliques.begin(), smallerCliques.end());
+					break; //all smallerCliques are the same size, so insert them all and break the loop
+				}
 				else
 				{
 					auto nextCliques = getPrevalentMaxCliques(c, prevalence);

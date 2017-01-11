30c9769946018f48c8ce82896ab3288304685d2c
Marek Rydlewski <marek.rydlewski@hotmail.com>
2017-01-11 14:43:25 +0100

    Fix duplicate cliques in parallel alg

--- GPUPatternMining/GPUPatternMining.CPU/CPUMiningAlgorithms/CPUMiningAlgorithmParallel.cpp
+++ GPUPatternMining/GPUPatternMining.CPU/CPUMiningAlgorithms/CPUMiningAlgorithmParallel.cpp
@@ -382,7 +382,10 @@ std::vector<std::vector<unsigned int>> CPUMiningAlgorithmParallel::getPrevalentM
 			for (auto c : smallerCliques)
 			{
 				if (c.size() == 2) //no need to construct tree, already checked by filterByPrevalence
+				{
 					finalMaxCliques.insert(finalMaxCliques.end(), smallerCliques.begin(), smallerCliques.end());
+					break; //all smallerCliques are the same size, so insert them all and break the loop
+				}
 				else
 				{
 					auto nextCliques = getPrevalentMaxCliques(c, prevalence);

3313e0503890d4601be31eafc146d4eee3723d59
kosiacz3q <kosiak.lukasz@gmail.com>
2017-01-12 05:16:25 +0100

    one test left

--- GPUPatternMining/GPUPatternMining/Prevalence/BitmapPairPrevalence.h
+++ GPUPatternMining/GPUPatternMining/Prevalence/BitmapPairPrevalence.h
@@ -3,13 +3,16 @@
 #include <map>
 
 #include <thrust/device_vector.h>
+#include <thrust/execution_policy.h>
+#include <thrust/unique.h>
 
 #include "../HashMap/gpuhashmapper.h"
 
 #include "../Entities/InstanceTable.h"
+#include "../Entities/FeatureTypePair.h"
+#include "../Entities/FeatureInstance.h"
 #include "../Entities/TypeCount.h"
 #include "../PlaneSweep/PlaneSweepTableInstanceResult.h"
-#include "../Entities/FeatureTypePair.h"
 //------------------------------------------------------------------
 
 class PlaneSweepTableInstanceResult;
@@ -26,6 +29,21 @@ namespace Prevalence
 		typedef std::shared_ptr<CountMap> CountMapPtr;
 		//------------------------------------------------------------------
 
+		struct FeatureInstancesTupleToFeatureTypePair : public thrust::unary_function<FeatureTypePair, thrust::tuple<FeatureInstance, FeatureInstance>>
+		{
+			__host__ __device__
+			FeatureTypePair operator()(thrust::tuple<FeatureInstance, FeatureInstance> ftt)
+			{
+				FeatureTypePair ftp;
+
+				ftp.combined =
+					((ftt.get<0>().fields.featureId << 16) & 0xFFFF0000)
+					| ((ftt.get<1>().fields.featureId) & 0x0000FFFF);
+
+				return ftp;
+			}
+		};
+
 		__global__
 		void setPrevalentFlag(
 			float minimalPrevalence
@@ -39,21 +57,47 @@ namespace Prevalence
 		__global__
 		void writeThroughtMask(
 			unsigned int count
-			, thrust::tuple<FeatureInstance, FeatureInstance>* uniques
+			, thrust::device_ptr<FeatureTypePair> uniques
 			, thrust::device_ptr<bool> mask
 			, thrust::device_ptr<unsigned int> writePos
 			, thrust::device_ptr<FeatureTypePair> prevalentPairs);
 		//------------------------------------------------------------------
+
+		struct UniqueTupleCountFunctor
+		{
+			thrust::device_ptr<FeatureInstance> data;
+			thrust::device_ptr<unsigned int> begins;
+			thrust::device_ptr<unsigned int> typeCount;
+			thrust::device_ptr<unsigned int> count;
+
+			thrust::device_ptr<FeatureInstance> uniquesOutput;
+
+			thrust::device_ptr<float> results;
+
+			__host__ __device__
+				void operator()(unsigned int idx)
+			{
+				results[idx] = thrust::distance(
+					uniquesOutput + begins[idx]
+					, thrust::unique_copy
+					(
+						thrust::device
+						, data + begins[idx]
+						, data + begins[idx] + count[idx]
+						, uniquesOutput + begins[idx]
+					)
+				) / static_cast<float>(typeCount[idx]);
+			}
+		};
+		// -------------------------------------------------------------------------------------------------
+
 
 		class BitmapPairPrevalenceCounter
 		{
 		public:
 
 			BitmapPairPrevalenceCounter(
-				CountMapPtr countMap
-				, InstanceTableMapPtr instanceTableMap
-				, unsigned int bitmapSize
-				, std::vector<TypeCount>& typesCounts
+				std::vector<TypeCount>& typesCounts
 			);
 
 			~BitmapPairPrevalenceCounter();
@@ -65,16 +109,13 @@ namespace Prevalence
 			uniques				-	vector that contains type pairs for prevalence check
 
 			*/
-			thrust::host_vector<FeatureTypePair> getPrevalentPairConnections(
+			thrust::device_vector<FeatureTypePair> getPrevalentPairConnections(
 				float minimalPrevalence
 				, PlaneSweepTableInstanceResultPtr planeSweepResult
 			);
 
 		private:
 
-			CountMapPtr gpuCountMap;
-			InstanceTableMapPtr instanceTableMap;
-			unsigned int bitmapSize;
 			std::map<unsigned int, unsigned short> typeCountMap;
 		};
 	}

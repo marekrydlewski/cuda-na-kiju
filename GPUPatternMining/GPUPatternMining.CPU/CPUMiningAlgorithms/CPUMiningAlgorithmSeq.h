02e2c0d7042c4141a41945e66fb468c84f1476bf
Marek Rydlewski <marek.rydlewski@hotmail.com>
2017-01-18 16:22:06 +0100

    Make maps faster

--- GPUPatternMining/GPUPatternMining.CPU/CPUMiningAlgorithms/CPUMiningAlgorithmSeq.h
+++ GPUPatternMining/GPUPatternMining.CPU/CPUMiningAlgorithms/CPUMiningAlgorithmSeq.h
@@ -5,7 +5,7 @@
 #include "../../GPUPatternMining.Contract/CliquesContainer.h"
 #include "../../GPUPatternMining.Contract/Enity/DataFeed.h"
 
-#include <map>
+#include <unordered_map>
 #include <vector>
 #include <set>
 
@@ -27,7 +27,7 @@ public:
 	void constructMaximalCliques() override;
 
 	std::vector<std::vector<unsigned short>> filterMaximalCliques(float prevalence);
-	std::map<unsigned short, std::map<unsigned short, std::map<unsigned short, std::vector<unsigned short>*>>> getInsTable()
+	std::unordered_map<unsigned short, std::unordered_map<unsigned short, std::unordered_map<unsigned short, std::vector<unsigned short>*>>> getInsTable()
 	{
 		return insTable;
 	}
@@ -44,9 +44,9 @@ private:
 	std::vector<int> typeIncidenceCounter;
 	/// InsTable - 2 dimensional hashtable, where frist 2 indexes are types
 	/// the value is a map, where key is number of 1st facility's instanceId and value is a vector of 2nd facility's instancesId 
-	std::map<unsigned short,
-		std::map<unsigned short,
-			std::map<unsigned short,
+	std::unordered_map<unsigned short,
+		std::unordered_map<unsigned short,
+			std::unordered_map<unsigned short,
 				std::vector<unsigned short>*>>> insTable;
 
 	Graph size2ColocationsGraph;

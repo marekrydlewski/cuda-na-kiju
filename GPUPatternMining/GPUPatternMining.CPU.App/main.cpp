f6677d4b712e05278ca877eae48e7248db01ede4
Marcin Jabłoński <jablon1994@gmail.com>
2017-01-25 22:33:14 +0100

    Added instance size tests

--- GPUPatternMining/GPUPatternMining.CPU.App/main.cpp
+++ GPUPatternMining/GPUPatternMining.CPU.App/main.cpp
@@ -8,36 +8,26 @@
 #include "../GPUPatternMining.Contract/SimulatedRealDataProvider.h"
 
 #include "../GPUPatternMining.CPU/CPUMiningAlgorithms/CPUMiningAlgorithmSeq.h"
-#include "../GPUPatternMining.CPU/CPUMiningAlgorithms/CPUMiningAlgorithmSeqV2.h"
 #include "../GPUPatternMining.CPU/CPUMiningAlgorithms/CPUMiningAlgorithmParallel.h"
 
 int main()
 {
 	//input data
-	const float threshold = 5;
-	const float prevalence = 0.1;
+	const float threshold = 3;
+	const float prevalence = 0.2;
 
 	SimulatedRealDataProvider dataProvider;
-	auto data = dataProvider.getTestData(DataSet::Fast);
+	auto data = dataProvider.getInstanceSizeTestData(InstanceSizeTestDataSet::Level8);
 
 	//output data
-	std::vector<std::vector<unsigned short>> solutionSeq, solutionSeq2, solutionParallel;
+	std::vector<std::vector<unsigned short>> solutionSeq, solutionParallel;
 
 	//////////////////////////////////////////////////
 	//benchmarking particular methods
 
 	CPUMiningAlgorithmSeq cpuAlgSeq;
-	CPUMiningAlgorithmSeqV2 cpuAlgSeq2;
 	CPUMiningAlgorithmParallel cpuAlgParallel;
-	bmk::benchmark<std::chrono::milliseconds> bmSeq, bmSeq2, bmParallel;
-
-	bmSeq2.run_p("load data", 1, [&]() { cpuAlgSeq2.loadData(std::get<0>(data), std::get<1>(data), std::get<2>(data)); });
-	bmSeq2.run_p("filter by distance", 1, [&]() { cpuAlgSeq2.filterByDistance(threshold); });
-	bmSeq2.run_p("filter by prevalence", 1, [&]() { cpuAlgSeq2.filterByPrevalence(prevalence); });
-	bmSeq2.run_p("construct max cliques", 1, [&]() { cpuAlgSeq2.constructMaximalCliques(); });
-	bmSeq2.run_p("filter max cliques", 1, [&]() { solutionSeq2 = cpuAlgSeq2.filterMaximalCliques(prevalence); });
-
-	bmSeq2.print("sequential algorithm 2", std::cout);
+	bmk::benchmark<std::chrono::milliseconds> bmSeq, bmParallel;
 
 	bmSeq.run_p("load data", 1, [&]() { cpuAlgSeq.loadData(std::get<0>(data), std::get<1>(data), std::get<2>(data)); });
 	bmSeq.run_p("filter by distance", 1, [&]() { cpuAlgSeq.filterByDistance(threshold); });
--- GPUPatternMining/GPUPatternMining.Contract/SimulatedRealDataProvider.h
+++ GPUPatternMining/GPUPatternMining.Contract/SimulatedRealDataProvider.h
@@ -5,14 +5,17 @@
 #include "Enity\DataFeed.h"
 
 enum class DataSet { VeryFast, Fast, Medium, Huge };
+enum class InstanceSizeTestDataSet { Level1, Level2, Level3, Level4, Level5, Level6, Level7, Level8 };
 
 class SimulatedRealDataProvider
 {
 private:
 	DataLoader loader;
 	static const std::map<DataSet, std::string> datasetNames;
+	static const std::map<InstanceSizeTestDataSet, std::string> instanceSizeTestDatasetNames;
 public:
 	std::tuple<DataFeed*, int, int> getTestData(DataSet dataset = DataSet::Fast);
+	std::tuple<DataFeed*, int, int> getInstanceSizeTestData(InstanceSizeTestDataSet dataset = InstanceSizeTestDataSet::Level1);
 };
 
 

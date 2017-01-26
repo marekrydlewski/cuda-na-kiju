d8912327ad44b75a3536348776f4061624b25c80
Marcin Jabłoński <jablon1994@gmail.com>
2017-01-26 23:00:07 +0100

    Removed unused things

--- GPUPatternMining/GPUPatternMining.Contract/SimulatedRealDataProvider.cpp
+++ GPUPatternMining/GPUPatternMining.Contract/SimulatedRealDataProvider.cpp
@@ -8,26 +8,9 @@ const std::map<DataSet, std::string> SimulatedRealDataProvider::datasetNames = {
 	{ DataSet::Huge, "\\Resource Files\\hugeData.txt" }
 };
 
-const std::map<InstanceSizeTestDataSet, std::string> SimulatedRealDataProvider::instanceSizeTestDatasetNames = {
-	{ InstanceSizeTestDataSet::Level1, "\\Resource Files\\instanceSizeTest1.txt" },
-	{ InstanceSizeTestDataSet::Level2, "\\Resource Files\\instanceSizeTest2.txt" },
-	{ InstanceSizeTestDataSet::Level3, "\\Resource Files\\instanceSizeTest3.txt" },
-	{ InstanceSizeTestDataSet::Level4, "\\Resource Files\\instanceSizeTest4.txt" },
-	{ InstanceSizeTestDataSet::Level5, "\\Resource Files\\instanceSizeTest5.txt" },
-	{ InstanceSizeTestDataSet::Level6, "\\Resource Files\\instanceSizeTest6.txt" },
-	{ InstanceSizeTestDataSet::Level7, "\\Resource Files\\instanceSizeTest7.txt" },
-	{ InstanceSizeTestDataSet::Level8, "\\Resource Files\\instanceSizeTest8.txt" }
-};
-
 std::tuple<DataFeed*, int, int> SimulatedRealDataProvider::getTestData(DataSet dataset)
 {
 	loader.loadFromTxtFile(ApplicationHelper::getCurrentWorkingDirectory() + datasetNames.at(dataset));
-	return std::make_tuple(loader.getData(), loader.getDataSize(), loader.getNumberOfTypes());
-}
-
-std::tuple<DataFeed*, int, int> SimulatedRealDataProvider::getInstanceSizeTestData(InstanceSizeTestDataSet dataset)
-{
-	loader.loadFromTxtFile(ApplicationHelper::getCurrentWorkingDirectory() + instanceSizeTestDatasetNames.at(dataset));
 	return std::make_tuple(loader.getData(), loader.getDataSize(), loader.getNumberOfTypes());
 }
 
--- GPUPatternMining/GPUPatternMining.Contract/SimulatedRealDataProvider.h
+++ GPUPatternMining/GPUPatternMining.Contract/SimulatedRealDataProvider.h
@@ -5,7 +5,6 @@
 #include "Enity\DataFeed.h"
 
 enum class DataSet { VeryFast, Fast, Medium, Huge };
-enum class InstanceSizeTestDataSet { Level1, Level2, Level3, Level4, Level5, Level6, Level7, Level8 };
 
 class SimulatedRealDataProvider
 {
@@ -15,7 +14,6 @@ private:
 	static const std::map<InstanceSizeTestDataSet, std::string> instanceSizeTestDatasetNames;
 public:
 	std::tuple<DataFeed*, int, int> getTestData(DataSet dataset = DataSet::Fast);
-	std::tuple<DataFeed*, int, int> getInstanceSizeTestData(InstanceSizeTestDataSet dataset = InstanceSizeTestDataSet::Level1);
 	std::tuple<DataFeed*, int, int> getTestData(std::string fileName);
 };
 

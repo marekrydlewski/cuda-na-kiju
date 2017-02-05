edd1d07af2cd9888549953026218cfbb714bba43
Marcin Jabłoński <jablon1994@gmail.com>
2017-02-05 19:08:22 +0100

    Removed unused provider methods

--- GPUPatternMining/GPUPatternMining.Contract/SimulatedRealDataProvider.h
+++ GPUPatternMining/GPUPatternMining.Contract/SimulatedRealDataProvider.h
@@ -4,15 +4,11 @@
 #include "DataLoader.h"
 #include "Enity\DataFeed.h"
 
-enum class DataSet { VeryFast, Fast, Medium, Huge };
-
 class SimulatedRealDataProvider
 {
 private:
 	DataLoader loader;
-	static const std::map<DataSet, std::string> datasetNames;
 public:
-	std::tuple<DataFeed*, int, int> getTestData(DataSet dataset = DataSet::Fast);
 	std::tuple<DataFeed*, int, int> getTestData(std::string fileName);
 };
 

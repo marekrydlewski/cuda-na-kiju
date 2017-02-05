f13be76d32202fd914bf69234777df63d1a781be
Marcin Jabłoński <jablon1994@gmail.com>
2017-02-05 21:08:32 +0100

    removed unused methods

--- GPUPatternMining/GPUPatternMining.Contract/SimulatedRealDataProvider.cpp
+++ GPUPatternMining/GPUPatternMining.Contract/SimulatedRealDataProvider.cpp
@@ -1,6 +1,19 @@
 #include "SimulatedRealDataProvider.h"
 #include "ApplicationHelper.h"
 
+const std::map<DataSet, std::string> SimulatedRealDataProvider::datasetNames = {
+	{ DataSet::VeryFast, "\\Resource Files\\veryFastData.txt" },
+	{ DataSet::Fast, "\\Resource Files\\fastData.txt" },
+	{ DataSet::Medium, "\\Resource Files\\mediumData.txt" },
+	{ DataSet::Huge, "\\Resource Files\\hugeData.txt" }
+};
+
+std::tuple<DataFeed*, int, int> SimulatedRealDataProvider::getTestData(DataSet dataset)
+{
+	loader.loadFromTxtFile(ApplicationHelper::getCurrentWorkingDirectory() + datasetNames.at(dataset));
+	return std::make_tuple(loader.getData(), loader.getDataSize(), loader.getNumberOfTypes());
+}
+
 std::tuple<DataFeed*, int, int> SimulatedRealDataProvider::getTestData(std::string fileName)
 {
 	loader.loadFromTxtFile(ApplicationHelper::getCurrentWorkingDirectory() + "\\Resource Files\\" + fileName + ".txt");
--- GPUPatternMining/GPUPatternMining.Contract/SimulatedRealDataProvider.h
+++ GPUPatternMining/GPUPatternMining.Contract/SimulatedRealDataProvider.h
@@ -4,11 +4,15 @@
 #include "DataLoader.h"
 #include "Enity\DataFeed.h"
 
+enum class DataSet { VeryFast, Fast, Medium, Huge };
+
 class SimulatedRealDataProvider
 {
 private:
 	DataLoader loader;
+	static const std::map<DataSet, std::string> datasetNames;
 public:
+	std::tuple<DataFeed*, int, int> getTestData(DataSet dataset = DataSet::Fast);
 	std::tuple<DataFeed*, int, int> getTestData(std::string fileName);
 };
 
